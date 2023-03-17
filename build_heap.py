a2952f63b4dd56094ec76975e77b496a21c253b6
Sefanovskis-Artjoms <artjoms200@gmail.com>
2023-03-17 19:04:28 +0200

    pirmais mēģinājums

--- build_heap.py
+++ build_heap.py
@@ -1,12 +1,26 @@
 # python3
 
-
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
+    for i in range(len(data),0,-1):
+        currentElementIndex = i
+        canSwap = True
+        while canSwap:
+            if currentElementIndex < 2:
+                canSwap = False
+                break
+            parentElement = data[currentElementIndex//2-1]
+            currentElement = data[currentElementIndex-1]
 
+            if currentElement < parentElement:
+                currentElement, parentElement = parentElement,currentElement
+                swaps.append([currentElementIndex//2-1,currentElementIndex-1])
+                currentElementIndex = currentElementIndex//2
+            else:
+                canSwap = False
     return swaps
 
 
@@ -16,10 +30,22 @@ def main():
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
 
+    # implement input form keyboard and from files
+    inpMethod = input()
+    # let user input file name to use, don't allow file names with letter a
+    if "F" in inpMethod:
+        fileName = input()
+        if "a" in fileName:
+            return
+        with open("./tests/" + fileName, "r") as f:
+            n = int(f.readline())
+            data = list(map(int,f.readline().split()))
+    elif "I" in inpMethod:
+        # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
@@ -34,8 +60,8 @@ def main():
 
     # output all swaps
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    for val in swaps:
+        print(val[0], val[1])
 
 
 if __name__ == "__main__":
